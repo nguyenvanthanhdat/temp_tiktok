d2389600e5837b18d03868c83c7741ef00cf1708
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-12-10 15:32:23 +0700

    Fix: not found package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='temp_tiktok',
     version='0.1',
-    packages=find_packages("Components"),
+    packages=find_packages(),
     install_requires=[
         # Add your project dependencies here
     ],
